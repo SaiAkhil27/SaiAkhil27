f3b8f2d301da7a015f8678f9e3480f84ba290dbb
SaiAkhil27 <133499734+SaiAkhil27@users.noreply.github.com>
2023-05-28 12:13:36 +0530

    Add files via upload

---
+++ exception_handling.java
@@ -0,0 +1,17 @@
+
+
+
+public class exception_handling {
+    public static void main(String[] args) {
+        try{
+            int data=50/0;
+        }
+        catch(ArithmeticException e){
+            System.out.println(e);
+        }
+        finally{
+            System.out.println("VIT");
+        }
+    }
+    
+}
